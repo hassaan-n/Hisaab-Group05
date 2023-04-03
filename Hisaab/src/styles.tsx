7d71ad94bab35221e4f0bf794198a37a57d04f6a
Queto99 <shajee2759@gmail.com>
2023-04-03 05:15:41 +0500

    Profile Pages for Settings

--- Hisaab/src/styles.tsx
+++ Hisaab/src/styles.tsx
@@ -108,6 +108,30 @@ const styles = StyleSheet.create({
     top: -35
   },
 
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  
+  },
+
   
 });
 
